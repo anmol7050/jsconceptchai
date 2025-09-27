3d18f56750f5ca590161ea9b28842e9afc7865d8
anmol7050 <anmolkumaryadav7050@gmail.com>
2025-09-27 13:31:35 +0530

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId=12345
+let accountEmail="anmol@gmai.com"
+var accountPassword="anmol1234"
+accountCity="patna"
+let anmol
+
+console.log(accountId)
+
+/*
+var ka use nahi karna hai kabhi bhi means never ever use var 
+use only let and const
+*/
+
+console.table([accountId, accountEmail, accountPassword, accountCity, anmol])
