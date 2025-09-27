0cd8be856c8dc403983d428dbc74b73f81bce279
anmol7050 <anmolkumaryadav7050@gmail.com>
2025-09-27 17:34:37 +0530

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,24 @@
+"use strict"; // treat all js code as newer version
+
+// alert(3 + 3) // yaha nahi run karega as we are using node js not browser
+
+console.log(3 + 3)
+
+console.log("Anmol")
+
+let name="Anmol"
+let age=5
+let isLoggedIn=false
+
+// number => 2 to power 53
+// bigint
+// string =>""
+// boolean =>true/false
+// null => standalone value
+// undefined 
+// symbol => when we have to talk about uniqueness (mostly used in react)
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null) // object
