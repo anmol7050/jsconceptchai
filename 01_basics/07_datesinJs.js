b111e3829c84db1149c40ab3d6f81527eee48535
anmol7050 <anmolkumaryadav7050@gmail.com>
2025-09-28 19:47:39 +0530

    dates

---
+++ 01_basics/07_datesinJs.js
@@ -0,0 +1,37 @@
+// Dates
+
+// let myDate=new Date()
+// console.log(myDate);
+
+// console.log(typeof myDate); // object
+
+
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toTimeString());
+// console.log(myDate.toJSON());
+
+// let myCreateDate=new Date(2025, 8, 28, 19, 32)
+// console.log(myCreateDate);
+// console.log(myCreateDate.toDateString());
+// console.log(myCreateDate.toLocaleString());
+
+// let myCreatedDate=new Date("2025-01-24")
+
+// console.log(myCreatedDate.toLocaleString());
+
+// let myTimeStamp=Date.now()
+
+// console.log(myTimeStamp);
+
+// convert milli-seconds in seconds
+
+// console.log(Math.floor((Date.now()/1000)))
+
+let newDate=new Date()
+
+console.log(newDate);
+console.log(newDate.getMonth()); // plus 1 as it gives index
+console.log(newDate.getDay()); // 0->sunday
