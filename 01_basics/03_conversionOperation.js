383ef4dff855c3b787fecc509db52b59a8c2b7ec
anmol7050 <anmolkumaryadav7050@gmail.com>
2025-09-28 12:01:34 +0530

    conversionofdatatype

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,10 +1,10 @@
 let score="33"
 
-console.log(typeof score)
-console.log(typeof(score))
+// console.log(typeof score)
+// console.log(typeof(score))
 let valueInNumber=Number(score)
-console.log(typeof valueInNumber)
-console.log(valueInNumber)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
 
 // "33" =>33
 // "33abc" => NaN
@@ -15,7 +15,7 @@ console.log(valueInNumber)
 let isLoggedIn=" "
 
 let booleanIsLoggedIn=Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn)
+// console.log(booleanIsLoggedIn)
 
 // 1=>true
 // 0=>false
@@ -26,5 +26,44 @@ console.log(booleanIsLoggedIn)
 let someNumber=33
 
 let valueInString=String(someNumber)
-console.log(valueInString)
-console.log(typeof valueInString)
+// console.log(valueInString)
+// console.log(typeof valueInString)
+
+
+// *****************************************  Operations  **********************************************
+
+let value =3
+let negValue=-value
+// console.log(value)
+// console.log(negValue)
+
+// console.log(3+3)
+// console.log(7-5)
+// console.log(3*3)
+// console.log(3**3)
+// console.log(6/3)
+// console.log(3%2)
+
+
+let str1="Hello"
+let str2=" Anmol"
+let str3=str1+str2
+// console.log(typeof str3)
+// console.log(str3)
+
+
+// console.log("1" + 2)
+// console.log(1 + "2")
+// console.log("1" + 3 + 3)
+// console.log(1 +3 + "4")
+
+
+// console.log(+true)
+// console.log(+"")
+
+let gameCounter=100
+gameCounter++
+console.log(gameCounter)
+
+
+
