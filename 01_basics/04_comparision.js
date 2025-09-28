b1a6d7d8ca27f44a686d95a9fd894bafa3f05817
anmol7050 <anmolkumaryadav7050@gmail.com>
2025-09-28 12:25:39 +0530

    comparision

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,33 @@
+// console.log(2>1)
+// console.log(2>=1)
+// console.log(2<1)
+// console.log(2<=1)
+// console.log(2==1)
+// console.log(2!=1)
+
+
+
+// console.log("2">1)
+// console.log("02"<1)
+
+// At the time of comparision js convert the string into number or we say value and then compare it with other
+
+// console.log(null>0)
+// console.log(null<0)
+// console.log(null==0);
+// console.log(null>=0);
+// console.log(null<=0);
+
+
+
+// console.log(undefined>0);
+// console.log(undefined<0);
+// console.log(undefined==0);
+
+// ===
+// (===) This is used to check the value as well as data type of the comparing entities
+console.log(2==="2");
+console.log("2"==="2");
+console.log(3===3);
+
+
