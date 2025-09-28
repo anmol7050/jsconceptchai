2878bccde95c24e5ccfdf52f18819f051da265f7
anmol7050 <anmolkumaryadav7050@gmail.com>
2025-09-28 16:59:12 +0530

    strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,54 @@
+const name="anmol "
+const repoCount=9
+
+// console.log(name + repoCount ) // Do not use this 
+
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+
+const gameName=new String('Anmol')
+
+// console.log(gameName[0]);
+// console.log(gameName[1]);
+// console.log(gameName[2]);
+// console.log(gameName[3]);
+// console.log(gameName[4]);
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length)
+// console.log(gameName.toUpperCase())
+// console.log(gameName.toLowerCase());
+
+// console.log(gameName.charAt(2))
+// console.log(gameName.indexOf('m'));
+
+
+// const newString=gameName.substring(0, 4)
+// console.log(newString);
+
+// let anotherString=gameName.slice(0, 5)
+// console.log(anotherString);
+// anotherString=gameName.slice(-1, 5)
+// console.log(anotherString);
+// anotherString=gameName.slice(-2, 5)
+// console.log(anotherString);
+// anotherString=gameName.slice(-3, 5)
+// console.log(anotherString);
+// anotherString=gameName.slice(-4, 5)
+// console.log(anotherString);
+// anotherString=gameName.slice(-5, 5)
+// console.log(anotherString)
+
+
+const newstr="       anmol     "
+console.log(newstr);
+console.log(newstr.trim());
+
+
+const url="https://anmol.com/anmol%20kumar"
+console.log(url.replace('%20', '-'));
+console.log(url.includes('anmol'));
+
+const newname="anmol-kumar-yadav"
+console.log(newname.split('-'));
