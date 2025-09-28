0ab5fba487b949dae591f23c451420fab4f6de6d
anmol7050 <anmolkumaryadav7050@gmail.com>
2025-09-28 16:02:45 +0530

    stack and heap

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -22,7 +22,7 @@ let userEmail; // (value here is undefined)
 const id=Symbol("123")
 const anotherId=Symbol("123")
 
-console.log(id===anotherId)
+// console.log(id===anotherId)
 
 // Reference (Non Primitive)
 // Array
@@ -41,9 +41,36 @@ const myFunction=function(){
   console.log("Hello World");
 }
 
-console.log(typeof myFunction) // function
-console.log(typeof myObj) // object
-console.log(typeof heroes) // object
-console.log(typeof null) // object
-console.log(typeof undefined) // undefined
+// console.log(typeof myFunction) // function
+// console.log(typeof myObj) // object
+// console.log(typeof heroes) // object
+// console.log(typeof null) // object
+// console.log(typeof undefined) // undefined
 
+
+// ************************************************** Stack And Heap Memory ************************************
+
+
+// Stack(Primitive)->copy milta hai data ka                     Heap(Non-Primitive)->reference milta hai data ka
+// stack->change copy me hota hai                               heap->change original data me hota hai
+
+let myName="anmolKumarYadav"
+
+let anotherName=myName
+
+anotherName="ANMOL"
+
+console.log(anotherName)
+console.log(myName)
+
+let userOne={
+  email:"user@google.com",
+  upi:"user@ibl",
+}
+
+let userTwo=userOne
+
+userTwo.email="anmol@gmail.com"
+
+console.log(userOne.email)
+console.log(userTwo.email)
